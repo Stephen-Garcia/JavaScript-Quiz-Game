d9685394b51a0e43e9e6ea8e35e6d16daea67d3a
Stephen Garcia <garciastephen5280@gmail.com>
2023-07-30 23:14:13 -0600

    started new quiz js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,1 +1,21 @@
-// js comming
+const questions = [{
+    question: 'Which is the biggest planet in our solar system?',
+    options: ['Earth', 'Mars', 'Jupiter', 'Saturn', 'Neptune'],
+    answer: 2,
+}, {
+    question:
+    options:
+    answer:
+}, {
+    question:
+    options:
+    answer:
+}, {
+    question:
+    options:
+    answer:
+}, {
+    question:
+    options:
+    answer:
+}];
